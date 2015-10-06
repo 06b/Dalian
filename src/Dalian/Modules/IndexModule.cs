f251c64ff03fdd75b2ef1b6cb5ee894f4a531b0f
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-10-05 22:12:13 -0400

    Add select.

--- src/Dalian/Modules/IndexModule.cs
+++ src/Dalian/Modules/IndexModule.cs
@@ -1,27 +1,78 @@
 ﻿using Dalian.Core.Helpers;
 using Dalian.Models;
 using Nancy;
+using Nancy.Extensions;
 using Nancy.ModelBinding;
 using NPoco;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Dynamic;
 
 namespace Dalian.Modules
 {
     public class IndexModule : NancyModule
     {
+        protected dynamic Model = new ExpandoObject();
+
         public IndexModule()
         {
+
+            IDatabase db = new Database(ConfigurationManager.ConnectionStrings["db"].Name);
+
             Get["/"] = _ =>
             {
-                return View["index"];
+                //TODO: Maybe create some sort of dashboard? Worse case - just redirect to /sites?
+                return "Dalian";
             };
 
+            /// <summary>
+            /// Retrieves all sites.
+            /// </summary>
             Get["/sites"] = _ =>
             {
-                return View["sites"];
+                List<Sites> sites = db.Fetch<Sites>();
+                db.Dispose();
+
+                Model = sites;
+
+                return View["index", Model];
             };
 
+            /// <summary>
+            /// Retrieves a specific site
+            /// </summary>
+            Get["/sites/{siteId}"] = parameter =>
+            {
+
+                string id = parameter.siteId;
+
+                var site = db.FetchBy<Sites>(sql => sql.Where(x => x.SiteId == id));
+
+                Model = site;
+
+                return View["index", Model];
+            };
+
+            /// <summary>
+            /// TODO: Update a specific site
+            /// </summary>
+            Put["/sites/{siteId}"] = parameter =>
+            {
+                throw new NotImplementedException();
+            };
+
+            /// <summary>
+            /// View for adding a new site
+            /// </summary>
+            Get["/sites/new"] = _ =>
+            {
+                return View["sites", Model];
+            };
+
+            /// <summary>
+            /// Creates a new site
+            /// </summary>
             Post["/sites"] = parameters =>
             {
                 Sites site = this.Bind<Sites>();
@@ -29,12 +80,10 @@ namespace Dalian.Modules
                 site.Active = true;
                 site.DateTime = DateTime.UtcNow;
 
-                IDatabase db = new Database(ConfigurationManager.ConnectionStrings["db"].Name);
                 db.Insert(site);
                 db.Dispose();
 
-
-                return View["sites"];
+                return this.Context.GetRedirect("~/sites/" + site.SiteId);
             };
         }
     }
