29f35eceddc3734169fc74f3c1ed0ebcb75444b2
Adrian D. Alvarez <adrian@adriandalvarez.com>
2016-03-27 22:05:15 -0400

    Add check if bookmark already exists

--- src/Dalian/Modules/IndexModule.cs
+++ src/Dalian/Modules/IndexModule.cs
@@ -47,18 +47,23 @@ namespace Dalian.Modules
                 }
             }
 
+            //Get the Title of the website
             meta.MetaTitle = document.DocumentNode.SelectSingleNode("//title").InnerText;
+
+            //Get MetaTags of the website
             var metaTags = document.DocumentNode.SelectNodes("//meta");
 
             if (metaTags != null)
             {
                 foreach (var tag in metaTags)
                 {
+                    //Check if Meta Description exists
                     if (tag.Attributes["name"] != null && tag.Attributes["content"] != null && tag.Attributes["name"].Value == "description")
                     {
                         meta.MetaDescription = tag.Attributes["content"].Value;
                     }
 
+                    //Check if Meta Keywords exist
                     if (tag.Attributes["name"] != null && tag.Attributes["content"] != null && tag.Attributes["name"].Value == "keywords")
                     {
                         meta.MetaKeywords = tag.Attributes["content"].Value;
@@ -69,8 +74,55 @@ namespace Dalian.Modules
             return meta;
         }
 
+        /// <summary>
+        /// Checks if bookmark already exists.
+        /// </summary>
+        /// <param name="Url">The URL.</param>
+        public static bool CheckIfBookmarkAlreadyExists(string Url)
+        {
+
+            if (Url.EndsWith("/", StringComparison.Ordinal))
+            {
+                //If the Url ends with a slash remove it
+                Url = Url.Substring(0, Url.Length - 1);
+            }
+
+            if (Url.StartsWith("http://", StringComparison.Ordinal))
+            {
+                //If Url starts with Http - Remove it
+                Url = Url.Substring(7);
+            }
+            else
+            {
+                //If Url starts with Https - Remove it
+                Url = Url.Substring(8);
+            }
+
+            var BookmarkWithHttpsWithoutSlash = "https://" + Url;
+            var BookmarkWithHttpsWithSlash = "https://" + Url + "/";
+            var BookmarkWithHttpWithoutSlash = "http://" + Url;
+            var BookmarkWithHttpWithSlash = "http://" + Url + "/";
+
+            IDatabase db = new Database(ConfigurationManager.ConnectionStrings["db"].Name);
+
+            //Check if the current requested URL already exists in the database with multiple combinations
+            var Bookmark = db.FetchBy<Sites>(sql => sql.Where(x => x.Url == BookmarkWithHttpsWithoutSlash || x.Url == BookmarkWithHttpsWithSlash || x.Url == BookmarkWithHttpWithoutSlash || x.Url == BookmarkWithHttpWithSlash)).ToList();
+            db.Dispose();
+
+            if (Bookmark.Count > 0)
+            {
+                //Bookmark exists
+                return true;
+            }
+
+            //Bookmark doesn't exist "in theory"
+            return false;
+
+        }
+
         public IndexModule()
         {
+
             IDatabase db = new Database(ConfigurationManager.ConnectionStrings["db"].Name);
 
             Get["/"] = _ =>
@@ -252,7 +304,11 @@ namespace Dalian.Modules
                     var bookmarks = reader.Read(file.Value);
                     foreach (var b in bookmarks.AllLinks)
                     {
-                        Debug.WriteLine("Type {0}, Title: {1}", b.GetType().Name, b.Title);
+                        //Don't create new bookmark if it already exists
+                        if (!CheckIfBookmarkAlreadyExists(b.Url))
+                        {
+                            Debug.WriteLine("Type {0}, Title: {1}", b.GetType().Name, b.Title);
+                        }
                     }
                 }
 
@@ -265,68 +321,86 @@ namespace Dalian.Modules
             Post["/sites"] = parameters =>
             {
                 Sites site = this.Bind<Sites>();
-                site.SiteId = ShortGuid.NewGuid().ToString();
-                site.Active = true;
-                site.DateTime = DateTime.UtcNow;
 
-                if (UriChecker.IsValidURI(site.Url))
+                //Don't create new bookmark if it already exists
+                if (!CheckIfBookmarkAlreadyExists(site.Url))
                 {
-                    SitesMeta metadata = GetMetaData(site.Url);
 
-                    site.MetaTitle = metadata.MetaTitle;
-                    site.MetaDescription = metadata.MetaDescription;
-                    site.MetaKeywords = metadata.MetaKeywords;
+                    site.SiteId = ShortGuid.NewGuid().ToString();
+                    site.Active = true;
+                    site.DateTime = DateTime.UtcNow;
+
+                    //Get the MetaData if it is a valid URI
+                    if (UriChecker.IsValidURI(site.Url))
+                    {
+                        SitesMeta metadata = GetMetaData(site.Url);
+
+                        site.MetaTitle = metadata.MetaTitle;
+                        site.MetaDescription = metadata.MetaDescription;
+                        site.MetaKeywords = metadata.MetaKeywords;
+                    }
+
+                    db.Insert(site);
+                    db.Dispose();
+
+                    //Get the tags for the bookmark and split them to a list
+                    string TagsPost = Request.Form.Tags.Value;
+                    List<string> Tags = TagsPost.ToLower().Split(',').Select(s => s.Trim()).ToList();
+
+                    //Get all of the existing tags
+                    List<Tags> AllTags = db.Fetch<Tags>();
+                    db.Dispose();
+
+                    //Get a list of the TagNames from of all the existing tags
+                    List<string> AllTagNames = AllTags.Select(tn => tn.TagName).ToList();
+
+                    //Check if the tag for the current bookmark already exists in the list of tags
+                    foreach (string tag in Tags)
+                    {
+                        //Add a new tag since it doesn't exist
+                        if (!AllTagNames.Contains(tag))
+                        {
+                            Tags newTag = new Tags();
+
+                            newTag.TagId = ShortGuid.NewGuid().ToString();
+                            newTag.TagName = tag;
+                            newTag.Active = true;
+
+                            db.Insert(newTag);
+
+                            //Link the tag with the site
+                            SiteTags newSiteTag = new SiteTags();
+                            newSiteTag.SiteId = site.SiteId;
+                            newSiteTag.TagId = newTag.TagId;
+
+                            db.Insert(newSiteTag);
+
+                            db.Dispose();
+                        }
+                        else
+                        {
+                            //Since the tag already exists, just link the tag with the site
+                            var ExistingTag = db.FetchBy<Tags>(sql => sql.Where(x => x.TagName == tag)).FirstOrDefault();
+
+                            SiteTags newSiteTag = new SiteTags();
+                            newSiteTag.SiteId = site.SiteId;
+                            newSiteTag.TagId = ExistingTag.TagId;
+
+                            db.Insert(newSiteTag);
+
+                            db.Dispose();
+                        }
+                    }
+
+
+                    //Redirect to the new site
+                    return Context.GetRedirect("~/sites/" + site.SiteId);
+
                 }
 
-                db.Insert(site);
-                db.Dispose();
+                    //TODO: Probably would be better to redirect to the existing page.
+                    return Response.AsRedirect("/sites");
 
-                string TagsPost = Request.Form.Tags.Value;
-                List<string> Tags = TagsPost.ToLower().Split(',').Select(s => s.Trim()).ToList();
-
-                List<Tags> AllTags = db.Fetch<Tags>();
-                db.Dispose();
-
-                List<string> AllTagNames = AllTags.Select(tn => tn.TagName).ToList();
-
-                foreach (string tag in Tags)
-                {
-                    if (!AllTagNames.Contains(tag))
-                    {
-                        Tags newTag = new Tags();
-
-                        newTag.TagId = ShortGuid.NewGuid().ToString();
-                        newTag.TagName = tag;
-                        newTag.Active = true;
-
-                        db.Insert(newTag);
-
-                        SiteTags newSiteTag = new SiteTags();
-                        newSiteTag.SiteId = site.SiteId;
-                        newSiteTag.TagId = newTag.TagId;
-
-                        db.Insert(newSiteTag);
-
-                        db.Dispose();
-                    }
-                    else
-                    {
-
-                        var ExistingTag = db.FetchBy<Tags>(sql => sql.Where(x => x.TagName == tag)).FirstOrDefault();
-
-                        SiteTags newSiteTag = new SiteTags();
-                        newSiteTag.SiteId = site.SiteId;
-                        newSiteTag.TagId = ExistingTag.TagId;
-
-                        db.Insert(newSiteTag);
-
-                        db.Dispose();
-                    }
-                }
-
-
-
-                return Context.GetRedirect("~/sites/" + site.SiteId);
             };
         }
     }
